bc4cac361b461346a96160f7738feb236cfb6e2e
Elier H <elierporto@gmail.com>
2022-01-07 18:24:13 -0500

    Add: dark mode session

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,71 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const getUserSettings = async () => {
+    //Get User Settings from DB
+    const res = await axios.get("/userSettings");
+    if (res.data.darkMode === true) {
+      setDarkMode(res.data.darkMode);
+    }
+  };
+
+  useEffect(() => {
+    //Check user settings on page load
+    try {
+      setIsLoading(true);
+      getUserSettings();
+    } catch (error) {
+      setIsError(true);
+    }
+    setIsLoading(false);
+  }, []);
+
+  const toggleDarkMode = async () => {
+    try {
+      setIsLoading(true);
+      //Creates dark mode session on backend
+      const res = await axios.post("/setDarkMode", {
+        darkMode: darkMode,
+      });
+      //Toggle dark mode to false
+      if (res.data.darkMode === true) {
+        setDarkMode(false);
+        await axios.post("/setDarkMode", {
+          darkMode: false,
+        });
+      }
+      //Toggle dark mode to true
+      if (res.data.darkMode === false) {
+        setDarkMode(true);
+        await axios.post("/setDarkMode", {
+          darkMode: true,
+        });
+      }
+    } catch (error) {
+      setIsError(true);
+    }
+    setIsLoading(false);
+  };
+
+  if (isError) return <h1>Error... could not change modes.</h1>;
+  if (isLoading) return <h1>Loading...</h1>;
   return (
     <div>
-      <h1>Hello World</h1>
+      {darkMode ? (
+        <h1>Currently in Dark Mode</h1>
+      ) : (
+        <h1>Currently in Light Mode</h1>
+      )}
+      {darkMode ? (
+        <button onClick={toggleDarkMode}>Light Mode</button>
+      ) : (
+        <button onClick={toggleDarkMode}>Dark Mode</button>
+      )}
     </div>
   );
 }
--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const express = require("express");
+const session = require("express-session");
 
 const port = 5000;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "SuperSecret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 app.get("/", (req, res) => {
   res.send("First Route");
+});
+
+app.get("/userSettings", (req, res) => {
+  res.send(req.session.userSettings);
+});
+
+app.post("/setDarkMode", (req, res) => {
+  //darkMode is True or False
+  const { darkMode } = req.body;
+  req.session.userSettings = { darkMode };
+  res.send(req.session.userSettings);
 });
 
 app.listen(port, () => console.log(`Listening at port ${port}!`));
